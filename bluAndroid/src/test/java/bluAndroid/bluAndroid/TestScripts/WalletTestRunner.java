aa667982406b29924c2717649fac68a4e12ad1ad
Urvashi <juhiurvashi@gmail.com>
2020-01-08 18:07:28 +0800

    Added Wallet Scripts

--- bluAndroid/src/test/java/bluAndroid/bluAndroid/TestScripts/WalletTestRunner.java
+++ bluAndroid/src/test/java/bluAndroid/bluAndroid/TestScripts/WalletTestRunner.java
@@ -48,15 +48,52 @@ public class WalletTestRunner extends BaseClass {
 		System.out.println("tc01_topUpWallet");
 		extentTest = extentReports.createTest("tc01_topUpWallet()");
 		w.clickOnWalletIcon();
+		String walletBalance=w.walletAmount().getText();
+		float f = Float.parseFloat(walletBalance);
+		float f1=f+5;
+		String floatingpoint = String.valueOf(f1);
 		w.clickOnTopUp();
+		w.selectValues();
+		w.selectPreferredMethod();
+		w.clickOnSubmit();
+		Assert.assertTrue(w.cardTextView().isDisplayed());
+		Assert.assertTrue(w.cardImageView().isDisplayed());
+		//Assert.assertEquals(w.dateTime().getText(), CommonUtil.getCorrectTimeDate());
+		System.out.println(w.topUpSuccessMsg().getText());
+		Assert.assertEquals(w.topUpSuccessMsg().getText(), "Top Up Successful");
+		Assert.assertTrue(w.dateTime().isDisplayed());
+		Assert.assertTrue(w.tickImageView().isDisplayed());
+		Assert.assertEquals(w.topUpAmount().getText(), "5.00");
+		w.clickOnclose();
+		String updatedValue=w.walletAmount().getText();
+		//float f1 = Float.parseFloat(walletBalance);
+		//float updatedValue1=f+5;
+		Assert.assertEquals(updatedValue, floatingpoint);
+		System.out.println(walletBalance+"  "+updatedValue);
 	}
 	@Test
-	public void tc02_payemntMethods()
+	public void tc02_paymentMethods()
 	{
 		System.out.println("tc02_payemntMethods");
 		extentTest = extentReports.createTest("tc02_payemntMethods");
 		w.clickOnWalletIcon();
 		w.clickOnPaymentMethods();
+		w.clickOnAddPaymentMethod();
+		w.enterCardNumber().sendKeys("42424242424242420420123");
+		w.clickOnSubmit();
+		Assert.assertTrue(w.cardTextView1().isDisplayed());
+		Assert.assertEquals(w.cardTextView1().getText(), "4242 (expires 04/20)");
+	}
+	@Test
+	public void tc02_removePaymentMethods()
+	{
+		System.out.println("tc02_payemntMethods");
+		extentTest = extentReports.createTest("tc02_payemntMethods");
+		w.clickOnWalletIcon();
+		w.clickOnPaymentMethods();
+		w.clickOnRemovePaymentMethod();
+		Assert.assertTrue(w.removePaymentmethodTitle().isDisplayed());
+		w.clickOnDeleteCardButton();
 	}
 	@Test
 	public void tc03_transactionDetails()
@@ -68,11 +105,37 @@ public class WalletTestRunner extends BaseClass {
 		w.clickOnSeeMore();
 		w.clickOnSearchBox().sendKeys("Booking");
 		w.transactionDetails();
-		List<WebElement> list=driver.findElements(By.id("//android.widget.ScrollView[@resource-id='sg.com.blu.android.uat.uat.uat:id/nestedScroll']//android.widget.LinearLayout"));
+		List<WebElement> list1=driver.findElements(By.xpath("//android.widget.ScrollView[@resource-id='sg.com.blu.android.uat:id/nestedScroll']//android.widget.LinearLayout//android.widget.LinearLayout//android.widget.TextView[1]"));
 		
-		  System.out.println(list.size()); for(int i=0;i<list.size();i++) {
-		  System.out.println(list.get(i).getText()); }
+		System.out.println(list1.size());
+		for (int i = 0; i < list1.size(); i++) {
+			System.out.println(list1.get(i).getText());
+			if(i==0)
+				Assert.assertEquals(list1.get(0).getText(), "Transaction ID");
+			if(i==1)
+				Assert.assertEquals(list1.get(1).getText(), "Date");
+			if(i==2)
+				Assert.assertEquals(list1.get(2).getText(), "Activity");
+			if(i==3)
+				Assert.assertEquals(list1.get(3).getText(), "Parcel reference");		
+		}
 		 
+	}
+	@Test
+	public void tc04_sendReceipt()
+	{
+		System.out.println("tc04_sendReceipt");
+		extentTest = extentReports.createTest("tc04_sendReceipt");
+		System.out.println("tc03_transactionDetails");
+		extentTest = extentReports.createTest("tc03_transactionDetails");
+		w.clickOnWalletIcon();
+		Assert.assertTrue(w.recentActivity().isDisplayed());
+		w.clickOnSeeMore();
+		w.clickOnSearchBox().sendKeys("Booking");
+		w.transactionDetails();
+		w.clickOnSendReceipt();
+		Assert.assertTrue(w.sendReceiptMsg().isDisplayed());
+		w.clickOnOK();
 	}
 	@AfterMethod
 	public void getResult(ITestResult testResult) throws IOException {
--- bluAndroid/src/test/java/bluAndroid/bluAndroid/pageObjects/Wallet.java
+++ bluAndroid/src/test/java/bluAndroid/bluAndroid/pageObjects/Wallet.java
@@ -55,7 +55,7 @@ public class Wallet {
 		}
 		public void transactionDetails()
 		{
-			WebElement list=driver.findElement(By.xpath("//android.widget.LinearLayout//android.widget.TextView[1]"));
+			WebElement list=driver.findElement(By.xpath("//androidx.recyclerview.widget.RecyclerView[@resource-id='sg.com.blu.android.uat:id/paymentHistoryRecylerView']//android.widget.RelativeLayout[1]"));
 			list.click();
 		/*
 		 * List<WebElement>
@@ -72,5 +72,95 @@ public class Wallet {
 		{
 			WebElement submit=driver.findElement(By.id("sg.com.blu.android.uat:id/submitButton"));
 			submit.click();
+		}
+		public void clickOnSendReceipt()
+		{
+			WebElement receipt=driver.findElement(By.id("sg.com.blu.android.uat:id/resendReceiptButton"));
+			receipt.click();
+		}
+		public WebElement sendReceiptMsg()
+		{
+			WebElement msg=driver.findElement(By.id("android:id/message"));
+			return msg;
+		}
+		public void clickOnOK()
+		{
+			WebElement ok=driver.findElement(By.id("android:id/button1"));
+			ok.click();
+		}
+		public void selectPreferredMethod()
+		{
+			WebElement preferredMethod=driver.findElement(By.xpath("//androidx.recyclerview.widget.RecyclerView[@resource-id='sg.com.blu.android.uat:id/paymentMethodsRecyclerView']//android.widget.LinearLayout"));
+			preferredMethod.click();
+		}
+		public WebElement topUpSuccessMsg()
+		{
+			WebElement msg=driver.findElement(By.xpath("//android.widget.ScrollView[@resource-id='sg.com.blu.android.uat:id/nestedScroll']//android.widget.LinearLayout//android.widget.TextView[1]"));
+			return msg;
+		}
+		public WebElement cardTextView()
+		{
+			WebElement card=driver.findElement(By.id("sg.com.blu.android.uat:id/cardTextView"));
+			return card;
+		}
+		public WebElement cardImageView()
+		{
+			WebElement card=driver.findElement(By.id("sg.com.blu.android.uat:id/cardBrandImageView"));
+			return card;
+		}
+		public WebElement tickImageView()
+		{
+			WebElement msg=driver.findElement(By.xpath("//android.widget.ScrollView[@resource-id='sg.com.blu.android.uat:id/nestedScroll']//android.widget.LinearLayout//android.widget.ImageView"));
+			return msg;
+		}
+		public WebElement topUpAmount()
+		{
+			WebElement amt=driver.findElement(By.id("sg.com.blu.android.uat:id/amountTextView"));
+			return amt;
+		}
+		public WebElement dateTime()
+		{
+			WebElement date=driver.findElement(By.id("sg.com.blu.android.uat:id/dateTimeTextView"));
+			return date;
+		}
+		public void clickOnclose()
+		{
+			WebElement close=driver.findElement(By.id("sg.com.blu.android.uat:id/closeButton"));
+			close.click();
+		}
+		public WebElement walletAmount()
+		{
+			WebElement date=driver.findElement(By.id("sg.com.blu.android.uat:id/walletBalanceTextView"));
+			return date;
+		}
+		public void clickOnRemovePaymentMethod()
+		{
+			WebElement remove=driver.findElement(By.xpath("//androidx.recyclerview.widget.RecyclerView[@resource-id='sg.com.blu.android.uat:id/paymentMethodsRecyclerView']//android.widget.LinearLayout[1]//android.widget.ImageButton[@resource-id='sg.com.blu.android.uat:id/deleteCardButton']"));
+			remove.click();
+		}
+		public void clickOnDeleteCardButton()
+		{
+			WebElement delete=driver.findElement(By.id("sg.com.blu.android.uat:id/deleteCardButton"));
+			delete.click();
+		}
+		public WebElement removePaymentmethodTitle()
+		{
+			WebElement title=driver.findElement(By.id("sg.com.blu.android.uat:id/title_tv"));
+			return title;
+		}
+		public void clickOnAddPaymentMethod()
+		{
+			WebElement add=driver.findElement(By.id("sg.com.blu.android.uat:id/addPaymentMethodContainer"));
+			add.click();
+		}
+		public WebElement enterCardNumber()
+		{
+			WebElement no=driver.findElement(By.id("sg.com.blu.android.uat:id/et_card_number"));
+			return no;
+		}
+		public WebElement cardTextView1()
+		{
+			WebElement card=driver.findElement(By.id("sg.com.blu.android.uat:id/cardNumberTextView"));
+			return card;
 		}
 }
